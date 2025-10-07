81e1ba84634e1f2ab98544ce451e581dfe8d249a
danielfsha <developedbydanielfisseha@gmail.com>
2025-10-07 22:16:28 +0300

    Feat: added a SubOrg and User type definition

--- src/transactions/types.ts
+++ src/transactions/types.ts
@@ -18,3 +18,40 @@ export type Wallet = {
     failure: null | object;
   };
 };
+
+export type User = {
+  userId: string;
+  userName: string;
+  userEmail: string;
+  authenticators: [];
+  apiKeys: [];
+  userTags: [];
+  oauthProviders: [];
+  createdAt: { seconds: string; nanos: string };
+  updatedAt: { seconds: string; nanos: string };
+};
+
+export type SubOrg = {
+  subOrganizationId: string;
+  wallet: {
+    walletId: string;
+    addresses: [string];
+  };
+  rootUserIds: string[];
+  activity: {
+    id: string;
+    organizationId: string;
+    status: string;
+    type: string;
+    intent: { createSubOrganizationIntentV7: [Object] };
+    result: { createSubOrganizationResultV7: [Object] };
+    votes: [[Object]];
+    appProofs: [];
+    fingerprint: string;
+    canApprove: false;
+    canReject: true;
+    createdAt: { seconds: string; nanos: string };
+    updatedAt: { seconds: string; nanos: string };
+    failure: null;
+  };
+};
